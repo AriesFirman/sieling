72a299c784ee4f89a8364ba584a9c700b070ac2e
yan043 <mahdian043@gmail.com>
2024-05-30 14:13:33 +0800

    added portal login

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -19,6 +19,8 @@ class ApiController extends Controller
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://tacticalpro.co.id/api/token/get',
             CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 0,
@@ -40,6 +42,12 @@ class ApiController extends Controller
         if (in_array($result->message, ['Success get token', 'Token was created']))
         {
             $token = $result->data->token;
+
+            DB::table('cookie_apps')
+            ->where('apps', 'tacticalpro')
+            ->update([
+                'token' => $token
+            ]);
         }
 
         return $token;
@@ -56,6 +64,8 @@ class ApiController extends Controller
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://tacticalpro.co.id/api/presensi/lensa',
             CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 0,
@@ -108,5 +118,185 @@ class ApiController extends Controller
                 sleep(1);
             }
         }
+    }
+
+    private function portal_login($user, $pass)
+    {
+        $curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://apps.telkomakses.co.id/portal/login.php',
+			CURLOPT_RETURNTRANSFER  => true,
+            CURLOPT_HEADER => true,
+			CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'GET',
+		));
+		$response = curl_exec($curl);
+		$header            = curl_getinfo($curl);
+		$header_content    = substr($response, 0, $header['header_size']);
+		trim(str_replace($header_content, '', $response));
+		$pattern           = "#Set-Cookie:\\s+(?<cookie>[^=]+=[^;]+)#m";
+		preg_match_all($pattern, $header_content, $matches);
+		$cookiesOut        = [];
+		$header['headers'] = $header_content;
+		$header['cookies'] = $cookiesOut;
+		$cookiesOut = implode("; ", $matches['cookie']);
+
+        DB::table('cookie_apps')
+        ->where('apps', 'portal')
+        ->update([
+            'cookies' => $cookiesOut
+        ]);
+
+		libxml_use_internal_errors(true);
+        $dom = new \DOMDocument();
+        $dom->loadHTML(trim($response));
+        $answer = $dom->getElementsByTagName("img")->item(1)->getAttribute('src');
+		$id_captcha = $dom->getElementById("id_captcha")->getAttribute("value");
+		$get_captcha_answer = DB::table('portal_captcha')->where('id_captcha', $id_captcha)->first();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://apps.telkomakses.co.id/portal/proses_login.php",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => "nik=$user&password=$pass&id_captcha=$id_captcha&answer=$get_captcha_answer->answer&submit=",
+			CURLOPT_HTTPHEADER => array(
+			  "Content-Type: application/x-www-form-urlencoded",
+			  "Cookie: ".$cookiesOut
+			),
+		));
+		curl_exec($curl);
+
+		$otp = 0;
+        print_r("\ninput otp:\n");
+        $handle = fopen ("php://stdin","r");
+        $line = fgets($handle);
+
+        if(trim($line) == 'cancel')
+        {
+            print_r("ABORTING!\n");
+            exit;
+        }
+
+        $otp = trim($line);
+        fclose($handle);
+        print_r("response $otp\n\n");
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://apps.telkomakses.co.id/portal/proses_otp.php",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => "otp=$otp&submit=",
+			CURLOPT_HTTPHEADER => array(
+			  "Content-Type: application/x-www-form-urlencoded",
+			  "Cookie: ".$cookiesOut
+			),
+		));
+		curl_exec($curl);
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://apps.telkomakses.co.id/portal/home.php",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HEADER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => "nik=$user&password=$pass&submit=",
+			CURLOPT_HTTPHEADER => array(
+			  "Content-Type: application/x-www-form-urlencoded",
+			  "Cookie: ".$cookiesOut
+			),
+		));
+		$response = curl_exec($curl);
+		libxml_use_internal_errors(true);
+        $dom = new \DOMDocument();
+        $dom->loadHTML(trim($response));
+
+        $linkElements = $dom->getElementsByTagName('a');
+		$targetKeyword = 'Presence & Leave';
+		$url_present = null;
+
+		foreach ($linkElements as $linkElement)
+		{
+			if (stripos($linkElement->textContent, $targetKeyword) !== false)
+			{
+				if ($linkElement->getAttribute('href') != 'hr.php?url=present')
+				{
+					$url_present = $linkElement->getAttribute('href');
+					break;
+				}
+			}
+		}
+
+		if ($url_present !== null)
+		{
+			print_r("Isi value dari href: $url_present\n");
+		}
+		else
+		{
+			print_r("Tidak ditemukan elemen dengan kata kunci '$targetKeyword'\n");
+		}
+
+        curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://apps.telkomakses.co.id/portal/save_user_log.php",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => "aplikasi=PRESENT&nik=$user",
+			CURLOPT_HTTPHEADER => array(
+			  "Content-Type: application/x-www-form-urlencoded",
+			  "Cookie: ".$cookiesOut
+			),
+		));
+		curl_exec($curl);
+
+        curl_setopt_array($curl, array(
+			CURLOPT_URL => $url_present,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HEADER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "GET",
+			CURLOPT_HTTPHEADER => array(
+				"Cookie: $cookiesOut"
+			),
+		));
+		curl_exec($curl);
     }
 }
